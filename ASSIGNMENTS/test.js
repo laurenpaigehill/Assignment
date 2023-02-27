4a530e4d52ae077d9e30cb2d08218653fd931d3c
“Lauren <laurenhillfreelance@gmail.com>
2023-02-27 19:18:26 +0000

    test

--- ASSIGNMENTS/test.js
+++ ASSIGNMENTS/test.js
@@ -1,8 +1,11 @@
-NUM_TRIALS = 1000;
+NUM_TRIALS = 10000;
 
 function drawOneCard() {
-  const suits = ["H"];
-  const numbers = ["2", "3"];
+  const suits = ["H", "C", "D", "H"];
+  const numbers = [
+    "2",
+    "3",
+  ];
   let oneSuit = Math.floor(Math.random() * suits.length);
   let oneNumber = Math.floor(Math.random() * numbers.length);
   let drawnCard = suits[oneSuit] + " " + numbers[oneNumber];
@@ -11,7 +14,7 @@ function drawOneCard() {
 
 function drawFiveCards() {
   let cards = [];
-  while (cards.length < 5) {
+  while (cards.length < 2) {
     let newCard = drawOneCard();
     if (!cards.includes(newCard)) {
       cards.push(newCard);
@@ -22,23 +25,14 @@ function drawFiveCards() {
 const hand = drawFiveCards();
 console.log(hand.join("\n"));
 
-// Define a function to check if a hand is a flush
-
-// creates a variabe that .maps through every element of the hand and implements the below function
-
-//  function is to split the elements in the array by a space and pulls the first element which is the suits
-
-// Uses the every() method to check if every element in the suits array is equal to the first element of the array.
-
-function isThreeOfAKind(hand) {
-  const ranks = hand.map((card) => card.split(" ")[1]);
-
-  for (let i = 0; i < ranks.length; i++) {
-    const count = ranks.filter((rank) => rank === ranks[i]).length;
-    if (count === 3) {
-      return "You have three of a kind!";
-    } else return "";
+function isStraight(hand) {
+  for (let i = 1; i < hand.length; i++) {
+    if (hand[i] !== hand[i - 1] + 1) {
+      return "";
+    }
   }
+  return "You have a Straight!";
 }
 
-console.log(isThreeOfAKind(hand));
+console.log(isStraight(hand));
+
